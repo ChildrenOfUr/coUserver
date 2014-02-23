cc0db8b5c399565950730a8b5ddb51cd33cf53d9
Robert McDermot <robert.mcdermot@gmail.com>
2014-02-23 16:21:41 -0500

    fix sending left message as string instead of map object

--- chatServer/chat_handler.dart
+++ chatServer/chat_handler.dart
@@ -134,7 +134,7 @@ class ChatHandler
 						leftForMessage["streetName"] = map["newStreet"];
 						leftForMessage["tsid"] = map["tsid"];
 						leftForMessage["message"] = " has left for ";
-						userSockets[id.username+"_"+"Local Chat"].add(leftForMessage);
+						userSockets[id.username+"_"+"Local Chat"].add(JSON.encode(leftForMessage));
 					}
 					if(id.currentStreet == map["newStreet"] && id.username != map["username"]) //others who are on the new street
 					{
