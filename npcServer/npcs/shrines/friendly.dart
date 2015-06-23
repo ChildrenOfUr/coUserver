3039ef20fd0395ad71a58f4e8dea82f5a6b2cc70
klikini <klikini@robiotic.net>
2015-06-23 13:52:29 -0500

    testing uppercase

--- npcServer/npcs/shrines/friendly.dart
+++ npcServer/npcs/shrines/friendly.dart
@@ -11,7 +11,7 @@ class Friendly extends Shrine
 				"still" : new Spritesheet("still","http://c2.glitch.bz/items/2012-12-06/npc_shrine_friendly__x1_open_png_1354831217.png",906,752,151,188,1,false)
 			};
 	 	currentState = states['still'];
-	 	type = 'friendly';
+	 	type = 'Friendly';
 
 	 	description = 'This is a shrine to Friendly, the giant who oversees all things celestial, nocturnal, lunar, stygian and murky. Despite this, he is, as his name implies, considered by many to be the nicest of the giants and the one most likely to loan you twenty currants till payday.';
 	}
