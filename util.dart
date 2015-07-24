8e6e58cb6584be2e2c5e03d2e99f4de0661cca45
Robert McDermot <robert.mcdermot@gmail.com>
2015-07-24 17:46:59 -0400

    receive layer images

--- util.dart
+++ util.dart
@@ -357,11 +357,32 @@ saveCacheToDisk(String filename, Map cache) async {
 	await file.writeAsString(JSON.encode(cache));
 }
 
-Future<bool> toast(String message, WebSocket userSocket) {
+void toast(String message, WebSocket userSocket) {
 	Map send = {
 		"toast": true,
 		"message": message
 	};
 	userSocket.add(JSON.encode(send));
-	return true;
+}
+
+@app.Route("/uploadStreetRender", methods: const [app.POST])
+uploadStreetRender(@app.Body(app.JSON) Map street) {
+	String tsid = street['tsid'];
+	if(tsid.startsWith('G')) {
+		tsid = 'L' + tsid.substring(1);
+	}
+	Map<String,String> layers = street['layers'];
+	new Directory('streetLayers/$tsid')..create();
+	layers.forEach((String layerName, String dataUri) async {
+		layerName = layerName.replaceAll(' ','_');
+		File layer = new File('streetLayers/$tsid/$layerName.png');
+		if(await layer.exists()) {
+			layer.delete();
+		}
+		layer.create();
+
+		dataUri = dataUri.substring(dataUri.indexOf(',')+1);
+		Image image = decodePng(CryptoUtils.base64StringToBytes(dataUri));
+		layer.writeAsBytes(encodePng(image));
+	});
 }
