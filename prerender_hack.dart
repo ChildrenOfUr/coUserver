c2f5057079e3cf0233116d134e693a15b22fabe2
Robert McDermot <robert.mcdermot@gmail.com>
2015-07-25 15:32:23 -0400

    better async/await logic so that the client waits for us to finish before sending more

--- prerender_hack.dart
+++ prerender_hack.dart
@@ -1,24 +1,35 @@
 part of coUserver;
 
 @app.Route("/uploadStreetRender", methods: const [app.POST])
-uploadStreetRender(@app.Body(app.JSON) Map street) {
-  String tsid = street['tsid'];
-  if(tsid.startsWith('G')) {
-    tsid = 'L' + tsid.substring(1);
-  }
-  Map<String,String> layers = street['layers'];
-  new Directory('streetLayers/$tsid')..create(recursive: true);
-  layers.forEach((String layerName, String dataUri) async {
-    layerName = layerName.replaceAll(' ','_');
-    print('creating $tsid');
-    File layer = new File('streetLayers/$tsid/$layerName.png');
-    if(await layer.exists()) {
-      layer.delete();
-    }
-    layer.create();
+Future<String> uploadStreetRender(@app.Body(app.JSON) Map street) async {
+	print('got ${street['tsid']}');
+	String tsid = street['tsid'];
+	if (tsid.startsWith('G')) {
+		tsid = 'L' + tsid.substring(1);
+	}
+	Map<String, String> layers = street['layers'];
+	Directory streetDir = new Directory('streetLayers/$tsid');
+	await streetDir.create(recursive: true);
 
-    dataUri = dataUri.substring(dataUri.indexOf(',')+1);
-    Image image = decodePng(CryptoUtils.base64StringToBytes(dataUri));
-    layer.writeAsBytes(encodePng(image));
-  });
+	List<Future> futures = [];
+	layers.forEach((String layerName, String dataUri) => futures.add(writeLayerToFile(tsid, layerName, dataUri)));
+	await Future.wait(futures);
+
+	print('finished ${street['tsid']}');
+	return 'saved';
+}
+
+Future writeLayerToFile(String tsid, String layerName, String dataUri) async {
+	layerName = layerName.replaceAll(' ', '_');
+	File layer = new File('streetLayers/$tsid/$layerName.png');
+	if (await layer.exists()) {
+		await layer.delete();
+	}
+	await layer.create();
+
+	dataUri = dataUri.substring(dataUri.indexOf(',') + 1);
+	Image image = decodePng(CryptoUtils.base64StringToBytes(dataUri));
+	await layer.writeAsBytes(encodePng(image));
+
+	print('saved $layerName for $tsid');
 }
