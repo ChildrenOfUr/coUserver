4d6d297cfe86e42cb6024f1a666b6c4520725a68
klikini <klikini@robiotic.net>
2015-09-10 20:10:52 -0500

    send bug reports to slack

--- common/report.dart
+++ common/report.dart
@@ -2,7 +2,7 @@ part of coUserver;
 
 @app.Group("/report")
 class Report {
-	static String reportUrl = "https://api.github.com/repos/ChildrenOfUr/cou-issues/issues";
+	static String issuesUrl = "ChildrenOfUr/cou-issues/issues";
 
 	@app.Route("/add", methods: const [app.POST], allowMultipartRequest: true)
 	Future addReport(@app.Body(app.FORM) Map data) async {
@@ -25,11 +25,19 @@ class Report {
 
 		// Get ready to send the data to GitHub
 
+		if (data["title"] == null) {
+			data["title"] = "Untitled issue";
+		}
+
 		Map assembleData = {
 			"title": data["title"],
 			"body": body,
-			"labels": ["type: ${data["category"]}", "status: pending"]
+			"labels": ["status: pending"]
 		};
+
+		if (data["category"] != null) {
+			(assembleData["labels"] as List<String>).insert(0, data["category"]);
+		}
 
 		String sendData = JSON.encode(assembleData);
 
@@ -39,6 +47,17 @@ class Report {
 			"Authorization": "token $githubToken"
 		};
 
-		await http.post(reportUrl, headers: headers, body: sendData);
+		http.Response ghReturn = await http.post("https://api.github.com/repos/$issuesUrl", headers: headers, body: sendData);
+
+		int newIssueId = JSON.decode(ghReturn.body)["number"];
+
+		// Notify in Slack
+
+		slack.Slack slackWebhook = new slack.Slack(slackReportWebhook);
+		slack.Message slackMessage = new slack.Message(
+			"New ${data["category"]}: ${data["title"]}"
+			"\nhttps://github.com/$issuesUrl/${newIssueId.toString()}"
+		);
+		slackWebhook.send(slackMessage);
 	}
 }
