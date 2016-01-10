0a38e9a74f17ce12002431221aa5c5eb6be2c706
klikini <klikini@robiotic.net>
2016-01-09 22:24:24 -0600

    upgrade classes & json

---
+++ gameServer/upgrades/upgrade_manager.dart
@@ -0,0 +1,26 @@
+part of coUserver;
+
+class UpgradeManager {
+	static Map<String, Upgrade> _upgrades;
+	static Map<String, Upgrade> get upgrades => _upgrades;
+
+	static List<Upgrade> get upgradesList => _upgrades.values;
+
+	static Future loadUpgrades() async {
+		try {
+			// Find the upgrades file
+			String directory = Platform.script.toFilePath();
+			directory = directory.substring(0, directory.lastIndexOf("/"));
+			File questsFile = new File("$directory/gameServer/upgrades/upgrades.json");
+
+			// Read the file
+			List<Upgrade> upgrades = decode(JSON.decode(await questsFile.readAsString()), Upgrade);
+
+			// Clear old upgrades data, then save the new
+			_upgrades = new Map();
+			upgrades.forEach((Upgrade upgrade) => _upgrades[upgrade.id] = upgrade);
+		} catch (e) {
+			log("Problem loading upgrades: $e");
+		}
+	}
+}
