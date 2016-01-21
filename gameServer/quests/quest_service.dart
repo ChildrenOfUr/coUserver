3848b305c9f3b70b9ef341c6588c2ef866a17421
Robert McDermot <robert.mcdermot@gmail.com>
2016-01-20 20:36:00 -0500

    remove debug messages

--- gameServer/quests/quest_service.dart
+++ gameServer/quests/quest_service.dart
@@ -82,15 +82,14 @@ class QuestService {
 		try {
 			String directory = Platform.script.toFilePath();
 			directory = directory.substring(0, directory.lastIndexOf('/'));
-			Directory dir = new Directory('$directory/gameServer/quests/json');
-			await dir.list().forEach((File questFile) async {
-				// load quest data
-				print('trying to load quest from ${questFile.path}');
-				Quest q = decode(JSON.decode(await questFile.readAsString()), Quest);
-				print('loaded quest ${q.id}');
-				quests[q.id] = q;
-			});
-
+			Directory questsDirectory = new Directory('$directory/gameServer/quests/json');
+			await for(FileSystemEntity entity in questsDirectory.list(recursive: true)) {
+				if (entity is File) {
+					// load quests
+					Quest q = decode(JSON.decode(await entity.readAsString()), Quest);
+					quests[q.id] = q;
+				}
+			}
 		}
 		catch (e) {
 			log("Problem loading quests: $e");
