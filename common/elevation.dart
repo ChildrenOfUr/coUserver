971846d3f6ad674a86247255123ccdd1046673ab
Andy <andy@robiotic.net>
2016-03-14 21:44:23 -0500

    syntax error

--- common/elevation.dart
+++ common/elevation.dart
@@ -59,7 +59,7 @@ class Elevation {
 
 		if (rows == 1) {
 			_cache[username] = elevation;
-			return "$username is now " (elevation != "" ? "a $elevation" : "demoted");
+			return "$username is now " + (elevation != "" ? "a $elevation" : "demoted");
 		} else {
 			return "An unknown error occurred. Try again?";
 		}
