a5b7f7fbe526374463c24486b536d8588320a824
Andy Castille <klikini@robiotic.net>
2016-06-06 11:21:26 -0500

    fixes https://github.com/ChildrenOfUr/cou-issues/issues/835 (pick up grapes)

---
+++ lib/entities/plants/respawning_items/helltomatoes.dart
@@ -0,0 +1,15 @@
+part of entity;
+
+class HellTomato extends RespawningItem {
+	HellTomato(String id, int x, int y, String streetName) : super(id, x, y, streetName) {
+		type = 'Tomato';
+		itemType = 'tomato';
+
+		states = {
+			'tomato': new Spritesheet('1-2-3-4', 'http://childrenofur.com/assets/entityImages/tomato__x1_1_x1_2_x1_3_x1_4_png_1354830045.png', 176, 31, 44, 31, 4, true)
+		};
+
+		setState('tomato');
+		maxState = 3;
+	}
+}
---
+++ lib/entities/plants/respawning_items/respawning_item.dart
@@ -0,0 +1,55 @@
+part of entity;
+
+abstract class RespawningItem extends Plant {
+	String itemType;
+
+	RespawningItem(String id, int x, int y, String streetName) : super(id, x, y, streetName) {
+		actions.add({
+			'action': 'pick up',
+			'actionWord': 'picking up',
+			'description': 'Take it for yourself',
+			'timeRequired': 0,
+			'enabled': true,
+			'requires': []
+		});
+
+		state = 0;
+		maxState = 0;
+	}
+
+	@override
+	void update() {
+		if (hidden && new DateTime.now().compareTo(respawn) >= 0) {
+			// Respawn now
+			show();
+			respawn = null;
+		}
+	}
+
+	bool get hidden => (respawn != null);
+
+	void show() {
+		state = 0;
+		setActionEnabled('pickUp', true);
+	}
+
+	void hide([Duration respawnIn]) {
+		state = maxState + 1;
+		setActionEnabled('pickUp', false);
+
+		if (respawnIn != null) {
+			respawn = new DateTime.now().add(respawnIn);
+		}
+	}
+
+	Future pickUp({WebSocket userSocket, String email}) async {
+		int added = await InventoryV2.addItemToUser(email, items[itemType].getMap(), 1);
+
+		if (added == 1) {
+			hide(new Duration(minutes: 1));
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
