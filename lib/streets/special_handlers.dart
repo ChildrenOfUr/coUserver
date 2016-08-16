149cda75688c7676e0f1f3a00db58b3befa30872
Andy Castille <klikini@robiotic.net>
2016-08-16 08:44:33 -0500

    here's your damn file ;)

---
+++ lib/streets/special_handlers.dart
@@ -0,0 +1,115 @@
+part of street_update_handler;
+
+class SavannaHandler {
+	// TODO: quest https://github.com/tinyspeck/glitch-GameServerJS/blob/f4cf3e3ed540227b0f1fec26dd5273c03b0f9ead/quests/baqala_nostalgia.js
+
+	// TODO: rock https://github.com/tinyspeck/glitch-GameServerJS/blob/f4cf3e3ed540227b0f1fec26dd5273c03b0f9ead/locations/savanna.js
+
+	static Future enter(String streetName, String email, WebSocket userSocket) async {
+		// Only when entering a Savanna street
+		if (!MapData.isSavannaStreet(streetName)) {
+			return;
+		}
+
+		if (!(await BuffManager.playerHasBuff('nostalgia_over', email))) {
+			// Allow
+			Log.verbose('<email=$email> entered $streetName in Savanna');
+			BuffManager.addToUser('nostalgia', email, userSocket);
+		} else {
+			// Disallow
+			toast('You are still too overwhelmed by nostalgia', userSocket);
+			_kick(streetName, email, userSocket);
+		}
+	}
+
+	static Future exit(String streetName, String email, WebSocket userSocket) async {
+		// Only when exiting a Savanna street
+		if (!MapData.isSavannaStreet(streetName)) {
+			return;
+		}
+
+		// Prevent entering again
+		Log.verbose('<email=$email> left Savanna');
+		_tempBan(email, userSocket);
+	}
+
+	static Future update(String streetName, String email, WebSocket userSocket) async {
+		// Waiting for the street to load prevents update() being called
+		// between enter() and the client loading the exit street
+		await new Future.delayed(new Duration(seconds: 10));
+
+		// Only when on a Savanna street
+		if (!MapData.isSavannaStreet(streetName)) {
+			return;
+		}
+
+		// Kick out players whose buffs have run out
+		if (!(await BuffManager.playerHasBuff('nostalgia', email))) {
+			toast('The nostalgia is overwhelming. You need to take a break', userSocket);
+			_kick(streetName, email, userSocket);
+		}
+	}
+
+	static Future _tempBan(String email, WebSocket userSocket) async {
+		// Switch buffs to prevent reentry
+		await Future.wait([
+			BuffManager.removeFromUser('nostalgia', email, userSocket),
+			BuffManager.addToUser('nostalgia_over', email, userSocket)
+		]);
+	}
+
+	static void _kick(String currentStreetName, String email, WebSocket userSocket) {
+		Log.verbose('Kicking <email=$email> from $currentStreetName in Savanna');
+
+		// Disallow entry
+		_tempBan(email, userSocket);
+
+		// Teleport to nearest non-Savanna hub
+		StreetUpdateHandler.teleport(
+			userSocket: userSocket,
+			email: email,
+			tsid: MapData.savannaEscapeTo(currentStreetName),
+			energyFree: true);
+	}
+}
+
+class WintryPlaceHandler {
+	static Future enter(String streetName, String email, WebSocket userSocket) async {
+		// Only when entering the Wintry Place
+		if (streetName != 'Wintry Place') {
+			return;
+		}
+
+		// Add warning buff
+		Log.verbose('<email=$email> entered Wintry Place');
+		await BuffManager.addToUser('cold_place', email, userSocket);
+	}
+
+	static Future exit(String streetName, String email, WebSocket userSocket) async {
+		// Only when exiting the Wintry Place
+		if (streetName != 'Wintry Place') {
+			return;
+		}
+
+		// Remove warning buff
+		Log.verbose('<email=$email> exited Wintry Place');
+		await BuffManager.removeFromUser('cold_place', email, userSocket);
+	}
+
+	static Future update(String streetName, String email) async {
+		// Only when in the Wintry Place
+		if (streetName != 'Wintry Place') {
+			return;
+		}
+
+		// Every 5 seconds, remove 4 energy (in addition to normal decay)
+		if (new DateTime.now().second % 5 == 0) {
+			Log.verbose('Removing energy from <email=$email> in Wintry Place');
+
+			// Write to database and send to client
+			Metabolics metabolics = await getMetabolics(email: email);
+			metabolics.energy -= 4;
+			await setMetabolics(metabolics);
+		}
+	}
+}
