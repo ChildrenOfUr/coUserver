c32f28de8f31def5b2dd94f743935457ad480c0e
Andy Castille <klikini@robiotic.net>
2016-08-16 23:33:14 -0500

    User class revamp with possibly breaking changes (expectations for returning null/empty user/error)

--- dbchanges.md
+++ dbchanges.md
@@ -1,1 +1,3 @@
 The following changes have been made to the dev database and will need to be added to the live database upon release.
+
+- ALTER TABLE users ADD COLUMN friends text NOT NULL DEFAULT '[]'::text;
--- lib/common/user.dart
+++ lib/common/user.dart
@@ -2,102 +2,173 @@ library user;
 
 import 'dart:async';
 
-import 'package:coUserver/common/util.dart';
-
 import 'package:redstone_mapper/mapper.dart';
 import 'package:redstone_mapper_pg/manager.dart';
 
+import 'package:coUserver/common/util.dart';
+
 class User {
 	@Field() int id;
-	@Field() String username, email, bio, achievements, elevation, custom_avatar;
+	@Field() String username;
+	@Field() String email;
+
+	/// user-written, displayed on profile
+	@Field() String bio;
+
+	/// JSON list of achv ids
+	@Field() String achievements;
+
+	/// guide, dev, ?
+	@Field() String elevation;
+
+	/// override avatar with another username
+	@Field() String custom_avatar;
+
+	/// JSON list of user ids
+	@Field() String friends;
+
+	/// true means the user cannot use global
 	@Field() bool chat_disabled;
+
+	/// last client connection
 	@Field() DateTime last_login;
 
-	static Map<String, String> _emailUsernames = {};
-	static Map<String, String> _usernameEmails = {};
-	static Map<int, String> _idUsernames = {};
-	static Map<String, int> _usernameIds = {};
+	static Future<int> getIdFromUsername(String username) async         => (await findByUsername(username)).id;
+	static Future<int> getIdFromEmail(String email) async               => (await findByEmail(email)).id;
+	static Future<String> getUsernameFromId(int id) async               => (await findById(id)).username;
+	static Future<String> getUsernameFromEmail(String email) async      => (await findByEmail(email)).username;
+	static Future<String> getEmailFromUsername(String username) async   => (await findByUsername(username)).email;
 
-	static void _updateMaps(User u) {
-		_emailUsernames[u.email] = u.username;
-		_idUsernames[u.id] = u.username;
-		_usernameEmails[u.username] = u.email;
-		_usernameIds[u.email] = u.id;
-	}
+	static Future<User> findById(int id, [bool cacheOnly = false]) async {
+		try {
+			return _cache.singleWhere((User u) => u.id != null && u.id == id);
+		} catch (_) {
+			if (cacheOnly) {
+				return null;
+			}
 
-	static Future<String> getUsernameFromEmail(String email) async {
-		if (_emailUsernames[email] != null) {
-			return _emailUsernames[email];
-		} else {
 			PostgreSql dbConn = await dbManager.getConnection();
 			try {
-				String query = "SELECT * FROM users WHERE email = @email";
-				User u = (await dbConn.query(query, User, {"email": email})).first;
-				_updateMaps(u);
-			} catch (e, st) {
-				Log.error('Error getting username for <email=$email>', e, st);
+				User user = (await dbConn.query(
+					'SELECT * FROM users WHERE id = @id',
+					User, {'id': id})).single;
+
+				_addToCache(user);
+				return user;
+			} catch (_) {
+				Log.warning('User <id=$id> not found');
+				return new User();
 			} finally {
 				dbManager.closeConnection(dbConn);
 			}
-
-			return _emailUsernames[email];
 		}
 	}
 
-	static Future<String> getEmailFromUsername(String username) async {
-		if (_usernameEmails[username] != null) {
-			return _usernameEmails[username];
-		} else {
+	static Future<User> findByUsername(String username, [bool cacheOnly = false]) async {
+		try {
+			return _cache.singleWhere((User u) => u.username != null && u.username == username);
+		} catch (_) {
+			if (cacheOnly) {
+				return null;
+			}
+
 			PostgreSql dbConn = await dbManager.getConnection();
 			try {
-				String query = "SELECT * FROM users WHERE username = @username";
-				User u = (await dbConn.query(query, User, {"username": username})).first;
-				_updateMaps(u);
-			} catch (e, st) {
-				Log.error('Error getting email for username $username', e, st);
+				User user = (await dbConn.query(
+					'SELECT * FROM users WHERE username = @username',
+					User, {'username': username})).single;
+
+				_addToCache(user);
+				return user;
+			} catch (_) {
+				Log.warning('User <username=$username> not found');
+				return new User();
 			} finally {
 				dbManager.closeConnection(dbConn);
 			}
-
-			return _usernameEmails[username];
 		}
 	}
 
-	static Future<String> getUsernameFromId(int id) async {
-		if (_idUsernames[id] != null) {
-			return _idUsernames[id];
-		} else {
+	static Future<User> findByEmail(String email, [bool cacheOnly = false]) async {
+		try {
+			return _cache.singleWhere((User u) => u.email != null && u.email == email);
+		} catch (_) {
+			if (cacheOnly) {
+				return null;
+			}
+
 			PostgreSql dbConn = await dbManager.getConnection();
 			try {
-				String query = "SELECT * FROM users WHERE id = @id";
-				User u = (await dbConn.query(query, User, {"id": id})).first;
-				_updateMaps(u);
-			} catch (e, st) {
-				Log.error('Error getting username for id $id', e, st);
+				User user = (await dbConn.query(
+					'SELECT * FROM users WHERE email = @email',
+					User, {'email': email})).single;
+
+				_addToCache(user);
+				return user;
+			} catch (_) {
+				Log.warning('User <email=$email> not found');
+				return new User();
 			} finally {
 				dbManager.closeConnection(dbConn);
 			}
-
-			return _idUsernames[id];
 		}
 	}
 
-	static Future<int> getIdFromEmail(String email) async {
-		if (_usernameIds[email] != null) {
-			return _usernameIds[email];
-		} else {
-			PostgreSql dbConn = await dbManager.getConnection();
-			try {
-				String query = "SELECT * FROM users WHERE email = @email";
-				User u = (await dbConn.query(query, User, {"email": email})).first;
-				_updateMaps(u);
-			} catch (e, st) {
-				Log.error('Error getting id for <email=$email>', e, st);
-			} finally {
-				dbManager.closeConnection(dbConn);
+	static void uncache({int id, String username, String email}) {
+		List<User> copy = new List.from(_cache);
+		_cache.forEach((User user) {
+			if (id != null && user.id == id) {
+				copy.remove(user);
 			}
 
-			return _usernameIds[email];
+			if (username != null && user.username == username) {
+				copy.remove(user);
+			}
+
+			if (email != null && user.email == email) {
+				copy.remove(user);
+			}
+		});
+		_cache = copy;
+	}
+
+	static List<User> _cache = [];
+
+	static Future _addToCache(User user) async {
+		// Find any possible duplicates
+		User byId = await findById(user.id, true);
+		User byUsername = await findByUsername(user.username, true);
+		User byEmail = await findByEmail(user.email, true);
+
+		// Use them to fill in any missing fields
+		_cache
+			..remove(byId)
+			..remove(byUsername)
+			..remove(byEmail);
+
+		// Store the resulting user
+		_cache.add(_mergeUsers([user, byId, byUsername, byEmail]));
+	}
+
+	/// Combine fields from all users to fill in any missing data.
+	/// Users earlier in the list will have priority in the case of duplicates.
+	static User _mergeUsers(List<User> users) {
+		User merged = new User();
+
+		for (User user in users) {
+			merged
+				..id = merged.id ?? user.id
+				..username = merged.username ?? user.username
+				..email = merged.email ?? user.email
+				..bio = merged.bio ?? user.bio
+				..achievements = merged.achievements ?? user.achievements
+				..elevation = merged.elevation ?? user.elevation
+				..custom_avatar = merged.custom_avatar ?? user.custom_avatar
+				..friends = merged.friends ?? user.friends
+				..chat_disabled = merged.chat_disabled ?? user.chat_disabled
+				..last_login = merged.last_login ?? user.last_login;
 		}
+
+		return merged;
 	}
 }
