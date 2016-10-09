602f921f6eb3903c56ae3a0078a2648793a7a73f
Andy Castille <klikini@robiotic.net>
2016-10-09 10:21:51 -0500

    re-add 3rd alchemy level, skill titles, verify skill state on startup

--- lib/skills/skillsmanager.dart
+++ lib/skills/skillsmanager.dart
@@ -36,7 +36,7 @@ class SkillManager extends Object {
 	static Future<int> loadSkills() async {
 		File file = new File(path.join(serverDir.path, 'lib', 'skills', 'skillsdata.json'));
 		JSON.decode(await file.readAsString()).forEach((String id, Map data) {
-			SKILL_DATA[id] = new Skill.fromMap(data, id);;
+			SKILL_DATA[id] = new Skill.fromMap(data, id);
 		});
 
 		_loading.complete();
