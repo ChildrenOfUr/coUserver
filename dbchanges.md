a983200a9962906aef0f790e47a119ec18bd8e64
RobertMcdermot <robert.mcdermot@gmail.com>
2016-10-20 21:00:05 -0400

    fixes ChildrenOfUr/cou-issues#1778 (Zilloween recipes), added smashing to get roasted pepitas

--- dbchanges.md
+++ dbchanges.md
@@ -1,12 +1,1 @@
 The following changes have been made to the dev database and will need to be added to the live database upon release.
-
-alter table street_entities add column h_flip boolean default false;
-alter table street_entities add column rotation int default 0;
-alter table stats add column rainbo_snocones_blended int not null default 0;
-
-Use id instead of username for note author: (RUN THESE IN ORDER!)
-
-UPDATE notes SET username = (SELECT id FROM users WHERE users.username = notes.username)::text;
-ALTER TABLE notes ALTER COLUMN username TYPE int USING username::integer;
-ALTER TABLE notes RENAME COLUMN username TO author_id;
-ALTER TABLE notes ADD FOREIGN KEY (author_id) REFERENCES users;
--- lib/entities/items/actions/itemgroups/pumpkins.dart
+++ lib/entities/items/actions/itemgroups/pumpkins.dart
@@ -6,10 +6,26 @@ abstract class Pumpkins extends Object with MetabolicsChange {
 
 		if (success) {
 			Item pumpkin = await InventoryV2.takeItemFromUser(email, map['slot'], map['subSlot'], 1);
-			String lanternType = pumpkin.itemType.replaceAll('_the_pumpkin', '_o_lantern');
+			String lanternType;
+			if (pumpkin.itemType == 'hello_pumpkin') {
+				lanternType = 'shiny_hello_pumpkin';
+			} else {
+				lanternType = pumpkin.itemType.replaceAll('_the_pumpkin', '_o_lantern');
+			}
 			await InventoryV2.addItemToUser(email, lanternType, 1);
 		} else {
 			toast("Sorry, you need more fireflies to light this up", userSocket);
+		}
+
+		return success;
+	}
+
+	Future<bool> smash({String streetName, Map map, WebSocket userSocket, String email, String username}) async {
+		bool success = (await InventoryV2.takeItemFromUser(email, map['slot'], map['subSlot'], 1)) != null;
+
+		if (success) {
+			int amount = rand.nextInt(8) + 3;
+			await InventoryV2.addItemToUser(email, 'roasted_pepitas', amount);
 		}
 
 		return success;
